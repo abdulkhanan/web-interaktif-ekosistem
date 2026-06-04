cc96c618ca0bada6a4e16b080a3201bba8ff5256
abdulkhanan <abdulkhanan.2025@student.uny.ac.id>
2026-06-04 09:34:47 +0700

    Update Admin.py

--- pages/Admin.py
+++ pages/Admin.py
@@ -184,11 +184,11 @@ def admin_navigation():
         '''
         <style>
             .admin-nav-spacer {
-                height: 40px;
+                height: 30px;
             }
             @media (max-width: 992px) {
                 .admin-nav-spacer {
-                    height: 40px;
+                    height: 30px;
                 }
             }
         </style>
